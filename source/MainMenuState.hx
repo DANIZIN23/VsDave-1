69bb0a12df2e334cd65cbaa288a09bd225c6eaf3
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-11-30 07:22:26 -0300

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -312,7 +312,11 @@ class MainMenuState extends MusicBeatState
 		// NG.core.calls.event.logEvent('swag').send();
 		
 
-		super.create();
+		#if android
+		addVirtualPad(LEFT_RIGHT, A_B_C);
+		#end	
+		
+			super.create();
 	}
 	
 	var selectedSomethin:Bool = false;
@@ -331,7 +335,7 @@ class MainMenuState extends MusicBeatState
 		}
 		if (canInteract)
 		{
-			if (FlxG.keys.justPressed.SEVEN)
+			if (FlxG.keys.justPressed.SEVEN #if android || FlxG.android.justReleased.BACK #end)
 			{
 				var deathSound:FlxSound = new FlxSound();
 				deathSound.loadEmbedded(Paths.soundRandom('missnote', 1, 3));
@@ -340,7 +344,7 @@ class MainMenuState extends MusicBeatState
 				
 				FlxG.camera.shake(0.05, 0.1);
 			}
-			if (FlxG.keys.justPressed.R)
+			if (FlxG.keys.justPressed.R #if android || virtualPad.buttonC.justPressed #end)
 			{
 				FlxG.sound.play(Paths.sound('scrollMenu'));
 				
